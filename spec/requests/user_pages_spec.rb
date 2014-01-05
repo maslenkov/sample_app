124c78cfab7b072300e83adc1d1ba5fe4d19d6bd
maslenkov <maslenkov@i.ua>
2014-01-05 17:02:44 +0200

    finished user updating

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -57,11 +57,50 @@ describe "User Pages" do
     end
   end
 
-  describe "profile page" do
+  context 'user exists' do
     let(:user) { FactoryGirl.create :user }
-    before { visit user_path(user) }
+    
+    describe "edit" do
+      before { visit edit_user_path(user) }
 
-    it { should have_selector('h1', text: user.name) }
-    it { should have_selector('title', text: user.name) }
+      describe "page" do
+        it { should have_selector('h1', text: "Update your profile") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+
+        it { should have_content('error') }
+      end
+
+      describe "with valid information" do
+        let(:new_name) { "New name" }
+        let(:new_email) { "new@example.com" }
+
+        before do
+          fill_in "Name", with: new_name
+          fill_in "Email", with: new_email
+          fill_in "Password", with: user.password
+          fill_in "Confirmation", with: user.password
+          click_button "Save changes"
+        end
+
+        it { should have_selector('title', text: new_name) }
+        it { should have_selector('div.alert.alert-success') }
+        it { should have_link('Sign out', href: signout_path) }
+
+        specify { user.reload.name.should == new_name }
+        specify { user.reload.email.should == new_email }
+      end
+    end
+
+    describe "profile page" do
+      before { visit user_path(user) }
+
+      it { should have_selector('h1', text: user.name) }
+      it { should have_selector('title', text: user.name) }
+    end
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,9 @@
 include ApplicationHelper
+
+def sign_in(user)
+  fill_in "Email", with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # for signining without capybara
+  cookies[:remember_token] = user.remember_token
+end
