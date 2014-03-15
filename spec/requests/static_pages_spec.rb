b9330c44b7c6e44e4e84f0eae23d517420a7eca8
maslenkov <maslenkov@i.ua>
2014-03-16 00:22:19 +0300

    add partials for follow/unfollow button their stat

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -57,6 +57,18 @@ describe "Static pages" do
           it { page.should have_content '2 microposts' }
         end
       end
+
+      describe "follower/following count" do
+        let(:other_user) { FactoryGirl.create :user }
+
+        before do
+          other_user.follow! user
+          visit root_path
+        end
+
+        it { should have_link "0 following", href: following_user_path(user) }
+        it { should have_link "1 followers", href: followers_user_path(user) }
+      end
     end
   end
 
