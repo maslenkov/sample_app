ee71ed71950edcb0d1e2e1f32e268c9a48c08c0e
maslenkov <maslenkov@i.ua>
2014-03-16 23:28:58 +0200

    update follow/unfollow feature to use AJAX

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,13 @@
 include ApplicationHelper
 
-def sign_in(user)
-  visit signin_path
-  fill_in "Email", with: user.email.upcase
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-  # for signining without capybara
-  cookies[:remember_token] = user.remember_token
+def sign_in(user, options = {})
+  if options[:no_capybara]
+    cookies[:remember_token] = user.remember_token
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+    cookies[:remember_token] = user.remember_token
+  end
 end
